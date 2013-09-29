ac06864ab1260ea6c734c7a906431c946b3355bb
Erik Beeson <erik.beeson@gmail.com>
2013-09-28 21:25:40 -0700

    Method to generate an observableArray of read/write KnockoutJS observables from the children of a Firebase instance.

--- firebase-knockout.js
+++ firebase-knockout.js
@@ -5,7 +5,7 @@
  */
 
 /**
- * Extend Firebase to generate a knockoutjs observable bound to the value of this ref.
+ * Extend Firebase to generate a KnockoutJS observable bound to the value of this ref.
  * 
  * This probably only makes sense to use on primitive values.
  * 
@@ -21,4 +21,37 @@ Firebase.prototype.asObservable = function() {
 		write: Firebase.prototype.set,
 		owner: this
 	});
+};
+
+
+/**
+ * Extend Firebase to generate a KnockoutJS observableArray bound to the children of this ref.
+ * 
+ * This probably only makes sense to use on a collection of primitive values.
+ * 
+ * Each array element is actually a `computed observable` that proxies writes to Firebase.
+ */
+Firebase.prototype.asObservableArray = function() {
+	var observableArray = ko.observableArray();
+
+	this.on('child_added', function(snapshot) {
+		var ref = snapshot.ref();
+		var observable = ko.observable(snapshot.val());
+		var computed = ko.computed({
+			read: observable,
+			write: Firebase.prototype.set,
+			owner: ref
+		});
+		observableArray.push(computed);
+		ref.on('value', function(snapshot) {
+			var value = snapshot.val();
+			if(value === null) {
+				observableArray.remove(computed);
+			} else {
+				observable(value);
+			}
+		});
+	});
+
+	return observableArray;
 };

c9431c89d047892b5ea4bc7815670e352cc698fc
Erik Beeson <erik.beeson@gmail.com>
2013-11-01 12:23:52 -0700

    Simplify firebase-codec to use specialized "percent encoding" instead of custom character escapes.

--- firebase-codec/firebase-codec.js
+++ firebase-codec/firebase-codec.js
@@ -1,22 +1,11 @@
-var firebaseCodec = (function() {
-	var escapes = (function(values) {
-		return function(initial, map) {
-			var i = values.length, result = initial;
-			while(i--) result = map(result, values[i]) || initial;
-			return result;
-		};
-	})([['\\', '\\'], ['.', '%'], ['#', '^'], ['$', '&'], ['[', '{'], [']', '}'] ]);
-
-	return {
-		encode: (function() {
-			var regexp = new RegExp('[' + escapes('', function(pattern, v) { return pattern + '\\' + v[0]; }) + ']', 'g');
-			var characterMap = escapes({}, function(map, v) { map[v[0]] = v[1]; });
-			return function(s) { return s.replace(regexp, function(c) { return '\\' + characterMap[c]; }); };
-		})(),
-		decode: (function() {
-			var regexp = new RegExp('\\\\[' + escapes('', function(pattern, v) { return pattern + '\\' + v[1]; }) + ']', 'g');
-			var characterMap = escapes({}, function(map, v) { map['\\' + v[1]] = v[0]; });
-			return function(s) { return s.replace(regexp, function(c) { return characterMap[c]; }); };
-		})()
+var firebaseCodec = {
+	encode: (function(pattern, replacer) {
+		return function(s) { return s.replace(pattern, replacer); };
+	})(/[/%\.#\$\[\]]/g, function(c) { return '%' + c.charCodeAt(0).toString(16).toUpperCase(); }),
+	encodeFully: function(s) {
+		return encodeURIComponent(s).replace('.', '%2E');
+	},
+	decode: function(s) {
+		return decodeURIComponent(s);
 	}
-})();
+};
--- firebase-codec/spec.js
+++ firebase-codec/spec.js
@@ -1,10 +1,12 @@
-describe('firebase-code tests', function() {
-	var reservedCharacters = ['.', '#', '$', '[', ']'];
+describe('firebase-codec tests', function() {
+	var reservedCharacters = ['.', '#', '$', '[', ']', '/'];
 	var testInputs = [
+		'\\/.#%[]$ ~!@(){}',
 		'foo.bar',
 		'boo[te$st]',
 		'\\one #{.\\\\',
-		'\\{'
+		'\\{',
+		'%24'
 	];
 
 	it('output matches input', function() {
@@ -15,9 +17,26 @@ describe('firebase-code tests', function() {
 		});
 	});
 
+	it('output matches input (fully)', function() {
+		testInputs.forEach(function(input) {
+			var encoded = firebaseCodec.encodeFully(input);
+			var decoded = firebaseCodec.decode(encoded);
+			expect(decoded).toBe(input);
+		});
+	});
+
 	it('encoded does not contain reserved characters', function() {
 		testInputs.forEach(function(input) {
 			var encoded = firebaseCodec.encode(input);
+			reservedCharacters.forEach(function(c) {
+				expect(encoded).not.toContain(c);
+			});
+		});
+	});
+
+	it('fully encoded does not contain reserved characters', function() {
+		testInputs.forEach(function(input) {
+			var encoded = firebaseCodec.encodeFully(input);
 			reservedCharacters.forEach(function(c) {
 				expect(encoded).not.toContain(c);
 			});
